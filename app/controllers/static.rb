ead783015993b01f1cfd4733b47bc6b07c3023ff
Mandych00ng <choong.mandy.yee@gmail.com>
2016-06-29 17:20:10 +0800

    looks nicer

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,26 +1,32 @@
-require 'byebug'
-require 'byebug'
 get '/' do
-@url = nil
-erb :"static/index"
+	@urls = Url.all
+	erb :"static/index"
 end
 
 post '/urls' do
-unless Url.exists?(long_url: params[:long_url])
-@url = Url.create(:long_url => params[:long_url], :short_url => Url.shorten)
+	@url = Url.find_by(long_url:params[:long_url])
 
+	if @url
+		@msg = "U ald exist!"
+		@urls = Url.all
+		erb :"static/index"
+		
 	else
-	Url.all.each do |u|
-	if u.long_url == params[:long_url]
-	@url = u
-	break
+		@url = Url.new(long_url:params[:long_url])
+		if @url.save
+			@msg = "TQ for your link, i ate it and u wait for the shorten one"
+			@urls = Url.all
+			erb :"static/index"
+		else
+			@msg = "wrong link la"
+			@urls = Url.all
+			erb :"static/index"
 		end
 	end
-	end
-		erb :"static/index"
-	end
+end
 
-	get '/:short_url' do
-	@url = Url.find_by(short_url: params[:short_url])
-	redirect 'http://' + @url.long_url
-	end
+get '/:short_url' do
+	@url = Url.where(short_url: params[:short_url]).first
+  @url.save
+  redirect @url.long_url
+end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,12 @@
 class Url < ActiveRecord::Base
+
+	validates :long_url, uniqueness:{case_sensitive: false, message: "Bad link!"}
+	validates :long_url, format:{with:(URI::regexp(['http', 'https']))}
 	
-	def  self.generate_short_url(url)
-		url.hash.abs.to_s(36)
+	before_save :shorten
+
+	def shorten
+		self.short_url = SecureRandom.hex(6)
 	end
 
 end
